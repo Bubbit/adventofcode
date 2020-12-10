3a66c153b1c5e284ae3b38982ef48519a7e0c641
Remco Gubbels <remco090@gmail.com>
2020-12-10 07:10:41 +0100

    feat: day 10 results

--- day10/day10.js
+++ day10/day10.js
@@ -1,1 +1,45 @@
 const { readInputFile } = require('./../util.js');
+
+export const countAdapterChain = async (inputFile) => {
+  const input = await readInputFile(inputFile);
+  const inputArray = input.map(num => Number(num)).sort((a, b) => a - b);
+  let ones = 0;
+  let threes = 0;
+  let lastAdapter = 0;
+
+  inputArray.forEach(element => {
+    const diff = element - lastAdapter;
+    if(diff === 1) {
+      ones++;
+    } else if(diff === 3 ) {
+      threes++;
+    }
+    lastAdapter = element;
+  });
+
+  return ones * (threes + 1);
+}
+
+export const countAdapterChains = async (inputFile) => {
+  const input = await readInputFile(inputFile);
+  const inputArray = input.map(num => Number(num)).sort((a, b) => a - b);
+  const valueOfLastAdapter = inputArray[inputArray.length - 1] + 3;
+  inputArray.unshift(0);
+  inputArray.push(valueOfLastAdapter);
+  const numWays = Object.fromEntries(inputArray.map((j) => [j, 0]));
+  const adapters = [...inputArray];
+
+  numWays[adapters[0]] = 1;
+
+  adapters.forEach((joltage) => {
+    const possibleJumps = inputArray.filter(
+      (jump) => jump > joltage && jump - joltage <= 3,
+    );
+
+    possibleJumps.forEach(jump => {
+      numWays[jump] += numWays[joltage];
+    });
+  });
+
+  return numWays[valueOfLastAdapter];
+}
--- day10/day10.spec.js
+++ day10/day10.spec.js
@@ -1,12 +1,25 @@
 const assert = require('assert');
 
-import { } from './day10';
+import { countAdapterChain, countAdapterChains } from './day10';
 
 describe('Day 10: ', () => {
   it('test', async () => {
-    // assert.strictEqual(await findSeat('BFFFBBFRRR'), 0);
+    assert.strictEqual(await countAdapterChain('day10/test.txt'), 35);
+  });
+  it('test 2 ', async () => {
+    assert.strictEqual(await countAdapterChain('day10/test2.txt'), 220);
   });
   it('input', async () => {
-    // assert.strictEqual(await findSeat('FFFBBBFRRR'), 0);
+    assert.strictEqual(await countAdapterChain('day10/input.txt'), 2263);
+  });
+
+  it('test options', async () => {
+    assert.strictEqual(await countAdapterChains('day10/test.txt'), 8);
+  });
+  it('test options 2 ', async () => {
+    assert.strictEqual(await countAdapterChains('day10/test2.txt'), 19208);
+  });
+  it('input options', async () => {
+    assert.strictEqual(await countAdapterChains('day10/input.txt'), 396857386627072);
   });
 });
