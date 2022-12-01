93e52f07c6ec788d6913369156163861b0883b6d
Remco Gubbels <DO20DQ@ing.com>
2022-12-01 11:46:53 +1100

    init 2022

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
 # adventofcode
+
+<!-- AOC TILES BEGIN -->
+<h1 align="center">
+  2021 - 36 ⭐
+</h1>
+<a href="None">
+  <img src="Media/2021/01.png" width="161px">
+</a>
+<a href="None">
+  <img src="Media/2021/02.png" width="161px">
+</a>
+<a href="None">
+  <img src="Media/2021/03.png" width="161px">
+</a>
+<a href="None">
+  <img src="Media/2021/04.png" width="161px">
+</a>
+<a href="None">
+  <img src="Media/2021/05.png" width="161px">
+</a>
+<a href="None">
+  <img src="Media/2021/06.png" width="161px">
+</a>
+<a href="None">
+  <img src="Media/2021/07.png" width="161px">
+</a>
+<a href="None">
+  <img src="Media/2021/08.png" width="161px">
+</a>
+<a href="None">
+  <img src="Media/2021/09.png" width="161px">
+</a>
+<a href="None">
+  <img src="Media/2021/10.png" width="161px">
+</a>
+<a href="None">
+  <img src="Media/2021/11.png" width="161px">
+</a>
+<a href="None">
+  <img src="Media/2021/12.png" width="161px">
+</a>
+<a href="None">
+  <img src="Media/2021/13.png" width="161px">
+</a>
+<a href="None">
+  <img src="Media/2021/14.png" width="161px">
+</a>
+<a href="None">
+  <img src="Media/2021/15.png" width="161px">
+</a>
+<a href="None">
+  <img src="Media/2021/16.png" width="161px">
+</a>
+<a href="None">
+  <img src="Media/2021/17.png" width="161px">
+</a>
+<a href="None">
+  <img src="Media/2021/18.png" width="161px">
+</a>
+<!-- AOC TILES END -->
+
---
+++ setup.sh
@@ -0,0 +1,48 @@
+
+year="2021"
+cookie=$(cat session.cookie)
+input_prefix="input"
+sample_prefix="example"
+
+# Example comes before input alphabetically, so it shows up in front in the program-tester.sh
+
+if [[ "day$1" ]]; then
+    if ! [[ -d "$year" ]]; then
+        echo "Creating folder for year $year"
+        mkdir "$year"
+    fi
+    cd "$year"
+
+    if ! [[ -d "day$1" ]]; then
+	echo "Creating folder for problem day$1"
+	mkdir "day$1"
+    fi
+    cd "day$1"
+
+    # Download input from advent of code
+    input_file="${input_prefix}.in"
+    if ! [[ -f "$input_file" ]]; then
+	echo "Downloading input for problem $1"
+	# Don't DDOS! So check if file exists already
+	curl "https://adventofcode.com/$year/day/$1/input" --cookie "session=$cookie" > "$input_file"
+    fi
+
+    # Add empty file so there are not that many lines when showing input instead
+    input_ans_file="${input_prefix}.ans"
+    if ! [[ -f "$input_ans_file" ]]; then
+	echo "Create empty input answer file"
+	touch "$input_ans_file"
+    fi
+
+    # Prepare dummy python solution
+    solution_file="day$1.py"
+    if ! [[ -f "$solution_file" ]]; then
+	echo "Create dummy python solution $solution_file"
+	cp "../../setup.py" "$solution_file"
+    fi
+
+
+    # nvim "$solution_file" "${sample_prefix}.in" "${sample_prefix}.ans" -c "norm G$"
+else
+    echo "Supply some number as first argument to initialize a problem!"
+fi
